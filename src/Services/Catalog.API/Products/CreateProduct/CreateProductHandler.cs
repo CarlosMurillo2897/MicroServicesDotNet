81a1f7002895c2d4b8f51f83627ffd8eb4b42812
Murillo Carlos <cmb2897@gmail.com>
2026-01-27 15:40:47 -0600

    Storing Products into DB. Configuring and setting DB connection. Updating Global Using with Marten and common libs. Adding Docker compose files.

--- src/Services/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using Marten;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
@@ -13,10 +9,6 @@ namespace Catalog.API.Products.CreateProduct
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // Create Product Entity from command object.
-            // Save to DB.
-
-
-            // Return result.
             var product = new Product
             {
                 Name = command.Name,
@@ -26,6 +18,11 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price
             };
 
+            // Save to DB.
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // Return result.
             return new CreateProductResult(product.Id);
         }
     }
