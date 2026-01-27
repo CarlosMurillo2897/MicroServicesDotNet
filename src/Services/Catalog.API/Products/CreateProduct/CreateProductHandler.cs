ad55f34d0a69ab6b720183f86f7572e8ad564b1b
Murillo Carlos <cmb2897@gmail.com>
2026-01-27 00:25:17 -0600

    Adding Marten pkg to work with PSQL to Catalog project. Configuring Carter/Mediatr in Program. Moving Carter from BuildingBlocks to Catalog project. Adding HTTP/HTTPS ports to launch Settings to match VS Docker file and HTTPS debug execution.

--- src/Services/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,19 +1,21 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
-using MediatR;
+using Marten;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : 
+    internal class CreateProductCommandHandler(IDocumentSession session) : 
         ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // Create Product Entity from command object.
             // Save to DB.
+
+
             // Return result.
             var product = new Product
             {
