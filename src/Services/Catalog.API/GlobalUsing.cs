17a6d180f1d2608337f9c57b5274a21011310fee
Murillo Carlos <cmb2897@gmail.com>
2026-01-28 17:21:59 -0600

    Seeding Data for Development environment.

--- src/Services/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog.API/GlobalUsing.cs
@@ -1,12 +1,13 @@
 ﻿global using Carter;
+global using Marten;
 global using Mapster;
 global using MediatR;
-global using Marten;
 global using FluentValidation;
 
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
 
+global using Catalog.API.Data;
 global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
