68de56130bb8dfcdf38d37f8c7d7d656c7b132a5
Murillo Carlos <cmb2897@gmail.com>
2026-01-29 07:52:46 -0600

    Creating BasketRepository and interface to implement Basket management. Defining ConnectionString for Marten. Defining ID Schema for Shopping Cart by using UserName. Adding BasketDB into Docker Compose files.

--- Services/Basket/Basket.API/GlobalUsing.cs
+++ Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,12 +1,15 @@
 ﻿// Nugget Packages.
+global using Marten;
 global using Carter;
 global using MediatR;
 global using Mapster;
 global using FluentValidation;
 
 // Building Blocks references.
-global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
 
 // Project references.
 global using Basket.API.Models;
+global using Basket.API.Exceptions;
