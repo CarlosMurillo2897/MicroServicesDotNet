74fbdd75d8752ed43610fceb9797a5d0473119a3
Murillo Carlos <cmb2897@gmail.com>
2026-01-28 19:06:51 -0600

    Adding Parameters to Get Products endpoint. Adding HealthChecks for PGSQL and Catalog API project. Updating Dockerfile. Adding Catalog API to Docker Compose.

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,9 +1,14 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the Container.
 builder.Services.AddCarter();
 
 var assembly = typeof(Program).Assembly;
+var DBConnectionString = builder.Configuration.GetConnectionString("Database");
+
 builder.Services.AddMediatR(config => {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
@@ -13,7 +18,7 @@ builder.Services.AddMediatR(config => {
 builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddMarten(opts => {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Connection(DBConnectionString!);
 }).UseLightweightSessions();
 
 if (builder.Environment.IsDevelopment())
@@ -21,11 +26,20 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(DBConnectionString!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
