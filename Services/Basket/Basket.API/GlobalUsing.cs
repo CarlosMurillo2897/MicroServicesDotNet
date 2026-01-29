7419c2d74f9fd36485049a48ffbdd259889161aa
Murillo Carlos <cmb2897@gmail.com>
2026-01-29 07:20:26 -0600

    Creating GetBasket and StoreBasket Endpoint/Handler. Adding Reference for BuildingBlocks. Adding GlobalUsing for Basket project.

---
+++ Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿// Nugget Packages.
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+
+// Building Blocks references.
+global using BuildingBlocks.CQRS;
+
+// Project references.
+global using Basket.API.Models;
--- src/Services/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog.API/GlobalUsing.cs
@@ -1,13 +1,16 @@
-﻿global using Carter;
+﻿// Nugget Packages.
+global using Carter;
 global using Marten;
 global using Mapster;
 global using MediatR;
 global using FluentValidation;
 
+// Building Blocks references.
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
 
+// Project references.
 global using Catalog.API.Data;
 global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
